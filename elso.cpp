e2c969501c1d4455fec70c756e9a9786cc2cac4a
Jesszika <47588049+Jesszika@users.noreply.github.com>
2019-09-24 19:27:49 +0300

    Add files via upload

---
+++ elso.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   double szam1, szam2;
+    cout << "szam1: ";
+    cin >> szam1;
+    cout << "szam2: ";
+    cin >> szam2;
+    cout << szam1 + szam2 << endl;
+
+    system("pause")
+    return 0;
+}
