6ab5c93363928eeae1b04699cd38ac7f080837fc
Jesszika <47588049+Jesszika@users.noreply.github.com>
2019-10-02 09:50:22 +0300

    Add files via upload

---
+++ kettesw.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int a=1, i=0;
+  int S=0;
+
+  while (a>=0) {
+    cout <<"a=";
+    cin >>a;
+  if (a=0 && a%2==0) {
+    S=S+a;
+   }
+
+}
+    cout << "az osszeg" << S;
+    return 0;
+}
