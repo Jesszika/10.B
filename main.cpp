d13d074a533f4a4675412807d637172f33307682
Jesszika <47588049+Jesszika@users.noreply.github.com>
2019-11-06 10:05:57 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,18 +4,36 @@ using namespace std;
 
 int main()
 {
- int a, i=1;
- double S=0;
+    int a, b, c, d, e, f, g, h, oszto;
+    cout << "e=";
+    cin >> e;
+    cout << "f=";
+    cin >> f;
 
- while (i<10){
-   cout << "a=";
-   cin >> a;
-   S=S+a;
-   i++;
 
- }
- S=S/10;
+    for (a=e; a<f; a++) {
+     d=0;
+     b=a;
 
-    cout << "az atlag" << S;
+    while (b>0) {
+     c=b % 10;
+     b=b / 10;
+     d=d*10+c;for (a=e; a<f; a++)
+     }
+     if (a==d) {
+       h=1;
+     }
+
+     for (oszto=2; oszto<a/2+1; oszto++) {
+     if (a%oszto==0) {
+        g=0;
+      }
+
+     }
+
+    }
+    if (h==1 && g==1) {
+    cout << a << endl;
+    }
     return 0;
 }
