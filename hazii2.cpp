3682840962747eecc2f0b927319aa5032365e5fa
Jesszika <47588049+Jesszika@users.noreply.github.com>
2020-01-29 10:49:32 +0200

    Add files via upload

---
+++ hazii2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main()
+{
+    int n, i, j, v[100000];
+    n=100;
+    for(i=0; i<n; i++) {
+      v[i]=rand()%1000;
+    }
+
+    return 0;
+}
