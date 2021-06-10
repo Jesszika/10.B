b7405f5fbf822c0ad8cd025bc142d2e56be3069f
Jesszika <47588049+Jesszika@users.noreply.github.com>
2021-06-10 16:30:10 +1000

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,30 +4,6 @@ using namespace std;
 
 int main()
 {
-  int tomb1[100];
-  int tomb2[100];
-  int i, c, d, j, N, M, S=0;
-  cout << "N=";
-  cin >> N;
-  cout << "M=";
-  cin >> M;
-  for (i=0; i<N; i++) {
-    cout << "tomb1["<< i << "]=";
-    cin >> tomb1[i];
-    }
-  for (i=0; i<M; i++) {
-    cout << "tomb2["<< j << "]=";
-    cin >> tomb2[j];
-    }
-  if (tomb1[i]==tomb2[j]) {
-    S+=tomb1[i];
-  }
-  while (S>0) {
-    c=S%10;
-    S=S/10;
-    d=d*10+c;
-  }
-
-    cout << "osszeg=" << S;
+    cout << "Hello world!" << endl;
     return 0;
 }
